764c933ddc73e304d8cd6f4a1937ec0b8bfc4cc5
jeyadevnarayanan <133682680+jeyadevnarayanan@users.noreply.github.com>
2024-05-18 15:12:37 +0530

    Update basic_gpu_op.py

--- chapter3/basic_gpu_op.py
+++ chapter3/basic_gpu_op.py
@@ -9,6 +9,6 @@ for device in ["/gpu:0", "/gpu:1"]:
         my_list.append(tf.matmul(x, y))
     with tf.device("/cpu:0"):
         sum_operation = tf.add(x,y)
-## Run everything through a session
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-sess.run(sum_operation)
+        ## Run everything through a session
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+        sess.run(sum_operation)
